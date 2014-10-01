8ab396a125d6890a6b9485d580932044972a0dc2
Stephen Sylvia <ssylvia@esri.com>
2014-10-01 12:09:00 -0400

    test app

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var router = express.Router();
+
+router.get('/',function(req,res){
+	res.send('It Works!');
+});
+
+router.get('/:name',function(req,res){
+	res.send('hello: ' + req.params.name);
+});
+
+app.use('/',router);
+
+app.listen(80);
+console.log('Listening on port 80');
